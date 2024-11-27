ffe031511ee30a8f2de571acc913abfd216ca47b
lh88790 <3467005771@qq.com>
2024-11-27 20:56:12 +0800

    Add files via upload

--- 瞎脚本.lua
+++ 瞎脚本.lua
@@ -57,14 +57,9 @@ credits:Slider('修改跳跃', 'Sliderflag',  16, 16, 999999,false, function(Val
 	loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))()
 end)
 
-credits:Button(
-        "换皮肤（需要别人id还需要加入群组Hell_$treet）",
-        function()
- loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\73\110\118\111\111\107\101\114\49\49\47\79\117\116\102\105\116\47\109\97\105\110\47\79\117\116\102\105\116\67\111\112\105\101\114\46\108\117\97\34\44\32\116\114\117\101\41\41\40\41\10")()
-        end)
         
         credits:Button(
-        "无限收益（copyid 输入别人名字获得id可搭配换皮肤使用）",
+        "指令脚本",
         function()
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
         end)
@@ -109,12 +104,6 @@ credits:Button(
         "无头和kor",
         function()
         loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Permanent-Headless-And-korblox-Script-4140"))()
-        end)
-        
-        credits:Button(
-        "光影",
-        function()
-        loadstring(game:HttpGet(('https://pastefy.app/xXkUxA0P/raw'),true))()
         end)
         
         credits:Button(
