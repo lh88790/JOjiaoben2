9db68e21419360b450ddc040336e5bad232d80ad
lh88790 <3467005771@qq.com>
2025-01-04 09:41:38 +0800

    Add files via upload

--- JO脚本.lua
+++ JO脚本.lua
@@ -584,10 +584,61 @@ Tab21:Button(
         end)
         
         credits:Button(
-        "刀刃球超强脚本",
+        "刀刃球自动弹反(点一下开启)",
         function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Akirascripts/Lunar/refs/heads/main/LuanrOnTop"))()
-        end)
+        local RunService = game:GetService("RunService") or game:FindFirstDescendant("RunService")
+local Players = game:GetService("Players") or game:FindFirstDescendant("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager") or game:FindFirstDescendant("VirtualInputManager")
+
+local Player = Players.LocalPlayer
+
+local Cooldown = tick()
+local IsParried = false
+local Connection = nil
+
+local function GetBall()
+  for _, Ball in ipairs(workspace.Balls:GetChildren()) do
+    if Ball:GetAttribute("realBall") then
+      return Ball
+    end
+  end
+end
+
+local function ResetConnection()
+    if Connection then
+        Connection:Disconnect()
+        Connection = nil
+    end
+end
+
+workspace.Balls.ChildAdded:Connect(function()
+    local Ball = GetBall()
+    if not Ball then return end
+    ResetConnection()
+    Connection = Ball:GetAttributeChangedSignal("target"):Connect(function()
+        Parried = false
+    end)
+end)
+
+RunService.PreSimulation:Connect(function()
+    local Ball, HRP = GetBall(), Player.Character.HumanoidRootPart
+    if not Ball or not HRP then
+      return
+    end
+    
+    local Speed = Ball.zoomies.VectorVelocity.Magnitude
+    local Distance = (HRP.Position - Ball.Position).Magnitude
+    
+    if Ball:GetAttribute("target") == Player.Name and not Parried and Distance / Speed <= 0.55 then
+      VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+      Parried = true
+      Cooldown = tick()
+      
+      if (tick() - Cooldown) >= 1 then
+        Partied = false
+      end
+    end
+end)
     
     local creds = window:Tab("奶酪逃生", "7733770599")
     local credits = creds:section("奶酪逃生", true)
