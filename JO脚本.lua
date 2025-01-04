886b622d040ecb0615ae4cc8a28bbacc79eeaf8a
lh88790 <3467005771@qq.com>
2025-01-04 10:12:26 +0800

    Add files via upload

--- JO脚本.lua
+++ JO脚本.lua
@@ -578,67 +578,15 @@ Tab21:Button(
     local credits = creds:section("刀刃球内容", true)
     
     credits:Button(
-        "刀刃球",
+        "刀刃球1",
         function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.Loader.lua", true))()
+        loadstring(game:HttpGet("https://pastebin.com/raw/xv50b6JL"))()
         end)
         
         credits:Button(
-        "刀刃球自动弹反(点一下开启)",
-        function()
-        local RunService = game:GetService("RunService") or game:FindFirstDescendant("RunService")
-local Players = game:GetService("Players") or game:FindFirstDescendant("Players")
-local VirtualInputManager = game:GetService("VirtualInputManager") or game:FindFirstDescendant("VirtualInputManager")
-
-local Player = Players.LocalPlayer
-
-local Cooldown = tick()
-local IsParried = false
-local Connection = nil
-
-local function GetBall()
-  for _, Ball in ipairs(workspace.Balls:GetChildren()) do
-    if Ball:GetAttribute("realBall") then
-      return Ball
-    end
-  end
-end
-
-local function ResetConnection()
-    if Connection then
-        Connection:Disconnect()
-        Connection = nil
-    end
-end
-
-workspace.Balls.ChildAdded:Connect(function()
-    local Ball = GetBall()
-    if not Ball then return end
-    ResetConnection()
-    Connection = Ball:GetAttributeChangedSignal("target"):Connect(function()
-        Parried = false
-    end)
-end)
-
-RunService.PreSimulation:Connect(function()
-    local Ball, HRP = GetBall(), Player.Character.HumanoidRootPart
-    if not Ball or not HRP then
-      return
-    end
-    
-    local Speed = Ball.zoomies.VectorVelocity.Magnitude
-    local Distance = (HRP.Position - Ball.Position).Magnitude
-    
-    if Ball:GetAttribute("target") == Player.Name and not Parried and Distance / Speed <= 0.55 then
-      VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 0)
-      Parried = true
-      Cooldown = tick()
-      
-      if (tick() - Cooldown) >= 1 then
-        Partied = false
-      end
-    end
-end)
+        "刀刃球2",
+        function()loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/Loader.lua"))())
+        end)
     
     local creds = window:Tab("奶酪逃生", "7733770599")
     local credits = creds:section("奶酪逃生", true)
